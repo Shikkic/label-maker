793611d6b06e4f3a4816fcc88db785d989189271
Dan Cadden <Shikkic@users.noreply.github.com>
2024-03-01 22:08:02 -0500

    Update LabelMakerPlugin.java

--- src/main/java/com/labelmaker/LabelMakerPlugin.java
+++ src/main/java/com/labelmaker/LabelMakerPlugin.java
@@ -3,8 +3,11 @@ package net.runelite.client.plugins.labelmaker;
 import com.google.inject.Provides;
 import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
+import net.runelite.client.RuneLite;
 import net.runelite.api.*;
 import net.runelite.api.Point;
+import net.runelite.api.coords.LocalPoint;
+import net.runelite.api.coords.WorldPoint;
 import net.runelite.api.events.GameStateChanged;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
@@ -14,10 +17,25 @@ import net.runelite.client.ui.overlay.Overlay;
 import net.runelite.client.ui.overlay.OverlayManager;
 import net.runelite.client.ui.overlay.OverlayPosition;
 import net.runelite.client.ui.overlay.OverlayPriority;
+import net.runelite.client.plugins.labelmaker.YOLOAnnotation;
+import net.runelite.client.util.ImageCapture;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+import java.util.ArrayList;
+import java.util.concurrent.ScheduledExecutorService;
+import net.runelite.client.ui.DrawManager;
+import java.util.UUID;
 
 import java.awt.*;
+import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
+import java.util.List;
+import java.util.function.Consumer;
 
 import net.runelite.client.ui.overlay.OverlayUtil;
+import net.runelite.client.util.ImageUtil;
 
 @Slf4j
 @PluginDescriptor(
@@ -33,6 +51,15 @@ public class LabelMakerPlugin extends Plugin
 
     @Inject
     private OverlayManager overlayManager;
+
+    @Inject
+    private ScheduledExecutorService executor;
+
+    @Inject
+    private ImageCapture imageCapture;
+
+    @Inject
+    private DrawManager drawManager;
 
     private ObjectIDOverlay objectIDOverlay;
 
@@ -77,31 +104,84 @@ public class LabelMakerPlugin extends Plugin
             setPriority(OverlayPriority.HIGH);
         }
 
+        void takeScreenshot(List<YOLOAnnotation> annotations)
+        {
+            String fileNameUUID = UUID.randomUUID().toString();
+            Consumer<Image> imageCallback = (img) ->
+            {
+                // This callback is on the game thread, move to executor thread
+                executor.submit(() -> saveScreenshot(fileNameUUID, "labelmaker", img, annotations));
+            };
+
+            drawManager.requestNextFrameListener(imageCallback);
+        }
+
+        public void writeAnnotationsToFile(String imageName, List<YOLOAnnotation> annotations) {
+            String filename = "C:\\Users\\shikkic\\Desktop\\osrsdata\\" + imageName + ".txt"; // Assuming image name is like 'image1.jpg'
+
+            try (FileWriter writer = new FileWriter(filename)) {
+                for (YOLOAnnotation annotation : annotations) {
+                    writer.write(annotation.toString() + "\n"); // Uses the toString() method
+                }
+            } catch (IOException e) {
+                System.err.println("Error writing annotations: " + e.getMessage());
+            }
+        }
+
+        private void saveScreenshot(String fileName, String subDir, Image image, List<YOLOAnnotation> annotations)
+        {
+            final BufferedImage screenshot;
+
+            screenshot = ImageUtil.bufferedImageFromImage(image);
+
+            imageCapture.saveScreenshot(screenshot, fileName, subDir, false, false);
+            writeAnnotationsToFile(fileName, annotations);
+        }
+
         @Override
         public Dimension render(Graphics2D graphics) {
-            Scene scene = client.getScene();
-            for (final Tile[] tiles : client.getScene().getTiles()[client.getPlane()])
-            {
-                for (final Tile tile : tiles) {
-                    if (tile == null) {
-                        continue;
-                    }
-
-                    for (final GameObject object : tile.getGameObjects()) {
-                        if (object == null) {
-                            continue;
-                        }
-                        if (object.getRenderable() instanceof Model) {
-                            Shape convexHull = object.getConvexHull();
-                            if (convexHull != null)
-                            {
-                                OverlayUtil.renderPolygon(graphics, convexHull, Color.RED);
-                            }
-
-                        }
-                    }
-                }
+            Player player = client.getLocalPlayer();
+            if (player == null) {
+                return null;
             }
+
+            LocalPoint playerLocation = client.getLocalPlayer().getLocalLocation();
+            if (playerLocation == null)
+            {
+                return null;
+            }
+
+            List<NPC> npcs = client.getNpcs();
+            List<YOLOAnnotation> annotations = new ArrayList<>();
+            for (NPC npc : npcs) {
+                Shape convexHull = npc.getConvexHull();
+                if (convexHull == null) {
+                    continue;
+                }
+                NPCComposition composition = npc.getComposition();
+                Rectangle2D aabb = convexHull.getBounds2D();
+
+                // Get bounding box coordinates
+                int x = (int) aabb.getMinX();
+                int y = (int) aabb.getMinY();
+                int width = (int) aabb.getWidth();
+                int height = (int) aabb.getHeight();
+
+                int categoryID = npc.getId();
+                if (categoryID != 2791) {
+                    continue;
+                }
+                if (x < 0 || y < 0) {
+                    continue;
+                }
+
+                annotations.add(new YOLOAnnotation(categoryID, x, y, width, height));
+
+                OverlayUtil.renderPolygon(graphics, aabb, Color.RED);
+            }
+
+            takeScreenshot(annotations);
+
             return null;
         }
     }
