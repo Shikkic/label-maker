05b329ea4489f94adbd2600adf46eed7a36809f0
Dan Cadden <Shikkic@users.noreply.github.com>
2024-05-18 06:58:04 -0400

    Update LabelMakerConfig.java

--- src/main/java/com/labelmaker/LabelMakerConfig.java
+++ src/main/java/com/labelmaker/LabelMakerConfig.java
@@ -4,16 +4,46 @@ import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
 
-@ConfigGroup("example")
+@ConfigGroup("labelmaker")
 public interface LabelMakerConfig extends Config
 {
-	@ConfigItem(
-			keyName = "greeting",
-			name = "Welcome Greeting",
-			description = "The message to show to the user when they login"
-	)
-	default String greeting()
-	{
-		return "Hello";
-	}
+    @ConfigItem(
+            keyName = "greeting",
+            name = "Welcome Greeting",
+            description = "The message to show to the user when they login"
+    )
+    default String greeting()
+    {
+        return "Hello";
+    }
+
+    @ConfigItem(
+            keyName = "debug bounding box",
+            name = "Show bounding boxes",
+            description = "TODO"
+    )
+    default boolean debugBBEnabled()
+    {
+        return false;
+    }
+
+    @ConfigItem(
+            keyName = "take screenshot",
+            name = "TODO",
+            description = "TODO"
+    )
+    default boolean screenshot()
+    {
+        return false;
+    }
+
+    @ConfigItem(
+            keyName = "nps",
+            name = "NPCs to label",
+            description = "TODo"
+    )
+    default String npcs()
+    {
+        return "";
+    }
 }
